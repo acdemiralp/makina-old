8f7dea05baaf270d1b8e469ed7130de6a43da169
Demiralp <demiralpali@gmail.com>
2018-03-10 00:44:47 +0100

    Added initial project structure, excluding conan integration.

---
+++ library/include/makina/makina.hpp
@@ -0,0 +1,17 @@
+#ifndef MAKINA_MAKINA_HPP_
+#define MAKINA_MAKINA_HPP_
+
+#include <makina/export.hpp>
+
+namespace makina
+{
+class MAKINA_EXPORT makina
+{
+public:
+
+protected:
+
+};
+}
+
+#endif
---
+++ library/source/makina.cpp
@@ -0,0 +1,1 @@
+#include <makina/makina.hpp>
