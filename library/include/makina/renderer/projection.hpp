62785546555284507ce3a862320b90f8b2da7ce7
Demiralp <demiralpali@gmail.com>
2018-03-12 00:47:27 +0100

    Added projection.

--- library/include/makina/renderer/projection.hpp
+++ library/include/makina/renderer/projection.hpp
@@ -1,6 +1,11 @@
 #ifndef MAKINA_RENDERER_PROJECTION_HPP_
 #define MAKINA_RENDERER_PROJECTION_HPP_
 
+#include <array>
+
+#include <di/utility/rectangle.hpp>
+#include <glm/mat4x4.hpp>
+
 #include <makina/export.hpp>
 
 namespace mak
@@ -8,9 +13,12 @@ namespace mak
 class MAKINA_EXPORT projection
 {
 public:
+  void             set_perspective(const float fov_y, const float aspect_ratio, std::array<float, 2> z_range);
+  void             set_orthogonal (const di::rectangle<float> rectangle       , std::array<float, 2> z_range);
+  const glm::mat4& matrix         () const;
 
 protected:
-
+  glm::mat4 matrix_;
 };
 }
 
--- library/source/renderer/projection.cpp
+++ library/source/renderer/projection.cpp
@@ -1,6 +1,19 @@
 #include <makina/renderer/projection.hpp>
 
+#include <glm/gtc/matrix_transform.hpp>
+
 namespace mak
 {
-
+void             projection::set_perspective(const float fov_y, const float aspect_ratio, std::array<float, 2> z_range)
+{
+  matrix_ = glm::perspective(fov_y, aspect_ratio, z_range[0], z_range[1]);
+}
+void             projection::set_orthogonal (const di::rectangle<float> rectangle       , std::array<float, 2> z_range)
+{
+  matrix_ = glm::ortho      (rectangle.left, rectangle.right, rectangle.bottom, rectangle.top, z_range[0], z_range[1]);
+}
+const glm::mat4& projection::matrix         () const
+{
+  return matrix_;
+}
 }
