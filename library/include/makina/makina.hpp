448679ef5e81eb042d14589517d777faf4a2fd49
Demiralp <demiralpali@gmail.com>
2018-03-11 19:40:41 +0100

    Added a make_default_engine function returning an engine with a display and an input system.

--- library/include/makina/makina.hpp
+++ library/include/makina/makina.hpp
@@ -1,43 +1,14 @@
 #ifndef MAKINA_MAKINA_HPP_
 #define MAKINA_MAKINA_HPP_
 
-#define NOMINMAX
-#include <assimp/mesh.h>
-#include <boost/signals2.hpp>
-#include <bm/bm.hpp>
-#include <btBulletCollisionCommon.h>
-#include <catch.hpp>
-#include <zmq.hpp>
-#include <di/engine.hpp>
-#include <ec/entity.hpp>
-#include <Eigen/Dense>
-#include <fg/render_task.hpp>
-#include <fi/image.hpp>
-#include <ft2build.h>
-#include <gl/opengl.hpp>
-#include <glm/glm.hpp>
-#include <imgui.h>
-#include <nlohmann/json.hpp>
-#include <foonathan/memory/container.hpp>
-#include <ra/registry.hpp>
-#include <rttr/registration.h>
-#include <spdlog/logger.h>
-#include <splinter/bspline.h>
-#include <tbb/task.h>
-#undef MemoryBarrier
-#include <vkhlf/CommandBuffer.h>
+#include <memory>
 
+#include <makina/core/engine.hpp>
 #include <makina/export.hpp>
 
-namespace makina
+namespace mak
 {
-class MAKINA_EXPORT makina
-{
-public:
-
-protected:
-
-};
+MAKINA_EXPORT std::unique_ptr<engine> make_default_engine();
 }
 
 #endif
--- library/source/makina.cpp
+++ library/source/makina.cpp
@@ -1,1 +1,15 @@
 #include <makina/makina.hpp>
+
+#include <makina/display/display_system.hpp>
+#include <makina/input/input_system.hpp>
+
+namespace mak
+{
+std::unique_ptr<engine> make_default_engine()
+{
+  auto   engine = std::make_unique<mak::engine>();
+  engine->add_system<display_system>();
+  engine->add_system<input_system  >();
+  return engine;
+}
+}
