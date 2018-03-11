503bab663fab689748a4373b8a444019be2f58b8
Demiralp <demiralpali@gmail.com>
2018-03-11 02:51:38 +0100

    Testing includes.

--- library/include/makina/makina.hpp
+++ library/include/makina/makina.hpp
@@ -1,6 +1,30 @@
 #ifndef MAKINA_MAKINA_HPP_
 #define MAKINA_MAKINA_HPP_
 
+#define NOMINMAX
+
+#include <assimp/mesh.h>
+#include <boost/signals2.hpp>
+#include <bm/bm.hpp>
+#include <btBulletCollisionCommon.h>
+#include <catch.hpp>
+#include <zmq.hpp>
+#include <di/engine.hpp>
+#include <ec/entity.hpp>
+#include <fg/render_task.hpp>
+#include <fi/image.hpp>
+#include <ft2build.h>
+#include <gl/opengl.hpp>
+#include <glm/glm.hpp>
+#include <imgui.h>
+#include <nlohmann/json.hpp>
+#include <foonathan/memory/container.hpp>
+#include <ra/registry.hpp>
+#include <rttr/registration.h>
+#include <spdlog/logger.h>
+#include <tbb/task.h>
+#include <vkhlf/CommandBuffer.h>
+
 #include <makina/export.hpp>
 
 namespace makina
