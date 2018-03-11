c100efcc9d4c818b43c53373def84cb04d3bb1d3
Demiralp <demiralpali@gmail.com>
2018-03-11 19:41:10 +0100

    Test demonstrates creation of a window and listening to input.

--- library/tests/makina_test.cpp
+++ library/tests/makina_test.cpp
@@ -1,6 +1,13 @@
 #include <catch.hpp>
 
+#include <makina/display/display_system.hpp>
+#include <makina/input/input_system.hpp>
+#include <makina/makina.hpp>
+
 TEST_CASE("Makina test.", "[makina]")
 {
-  
+  auto engine = mak::make_default_engine();
+  engine->get_system<mak::display_system>()->create_window("Makina", std::array<std::size_t, 2>{32, 32}, std::array<std::size_t, 2>{640, 480});
+  engine->get_system<mak::input_system>  ()->on_quit.connect(std::bind(&mak::engine::stop, engine.get()));
+  engine->run();
 }
