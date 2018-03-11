629fdd294e79b1834066499b982249bc025b3b0d
Demiralp <demiralpali@gmail.com>
2018-03-11 18:34:28 +0100

    Implemented engine class.

--- library/include/makina/core/engine.hpp
+++ library/include/makina/core/engine.hpp
@@ -1,23 +1,78 @@
 #ifndef MAKINA_CORE_ENGINE_HPP_
 #define MAKINA_CORE_ENGINE_HPP_
 
+#include <algorithm>
+#include <memory>
+#include <type_traits>
+#include <vector>
+
 #include <makina/export.hpp>
 #include <makina/core/frame_timer.hpp>
+#include <makina/core/logger.hpp>
+#include <makina/core/system.hpp>
 
 namespace mak
 {
+// An engine is a container of and is responsible for the updating of systems.
 class MAKINA_EXPORT engine
 {
 public:
   engine           ()                    = default;
-  engine           (const engine&  that) = default;
+  engine           (const engine&  that) = delete ;
   engine           (      engine&& temp) = default;
   virtual ~engine  ()                    = default;
-  engine& operator=(const engine&  that) = default;
+  engine& operator=(const engine&  that) = delete ;
   engine& operator=(      engine&& temp) = default;
 
+  template<typename system_type, typename... system_arguments>
+  system_type* add_system   (system_arguments&&... arguments)
+  {
+    static_assert(std::is_base_of<system, system_type>::value, "The type does not inherit from system.");
+
+    systems_.push_back(std::make_unique<system_type>(arguments...));
+    return static_cast<system_type*>(systems_.back().get());
+  }
+  template<typename system_type>
+  system_type* get_system   ()
+  {
+    static_assert(std::is_base_of<system, system_type>::value, "The type does not inherit from system.");
+
+    auto iterator = std::find_if(systems_.begin(), systems_.end(), system_type_predicate<system_type>);
+    if  (iterator == systems_.end())
+    {
+      logger->warn("System does not exist within the engine.");
+      return nullptr;
+    }
+    return static_cast<system_type*>(iterator->get());
+  }
+  template<typename system_type>
+  void         remove_system()
+  {
+    static_assert(std::is_base_of<system, system_type>::value, "The type does not inherit from system.");
+
+    auto iterator = std::remove_if(systems_.begin(), systems_.end(), system_type_predicate<system_type>);
+    if  (iterator == systems_.end())
+    {
+      logger->warn("System does not exist within the engine.");
+      return;
+    }
+    systems_.erase(iterator, systems_.end());
+  }
+
+  void run       ();
+  void stop      ();
+  bool is_running() const;
+
 protected:
-  frame_timer<float, std::milli> frame_timer_;
+  template<typename system_type>
+  static bool system_type_predicate(const std::unique_ptr<system>& iteratee)
+  {
+    return typeid(system_type) == typeid(*iteratee.get());
+  }
+
+  std::vector<std::unique_ptr<system>> systems_    ;
+  frame_timer<float, std::milli>       frame_timer_;
+  bool                                 is_running_ = false;
 };
 }
 
--- library/source/core/engine.cpp
+++ library/source/core/engine.cpp
@@ -2,5 +2,36 @@
 
 namespace mak
 {
+void engine::run       ()
+{
+  if (is_running_)
+  {
+    logger->warn("No-op. Engine is already running.");
+    return;
+  }
 
+  is_running_ = true;
+
+  frame_timer_.tick();
+  while (is_running_)
+  {
+    frame_timer_.tick();
+    for (auto& system : systems_)
+      system->update();
+  }
+}
+void engine::stop      ()
+{
+  if (!is_running_)
+  {
+    logger->warn("No-op. Engine is already stopped.");
+    return;
+  }
+
+  is_running_ = false;
+}
+bool engine::is_running() const
+{
+  return is_running_;
+}
 }
