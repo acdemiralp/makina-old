0f716d7cb0112c7bcc9a859341aa115e55cda30e
Demiralp <demiralpali@gmail.com>
2018-03-11 16:22:07 +0100

    Engine now has a frame timer.

--- library/include/makina/core/engine.hpp
+++ library/include/makina/core/engine.hpp
@@ -2,15 +2,22 @@
 #define MAKINA_CORE_ENGINE_HPP_
 
 #include <makina/export.hpp>
+#include <makina/core/frame_timer.hpp>
 
 namespace mak
 {
 class MAKINA_EXPORT engine
 {
 public:
+  engine           ()                    = default;
+  engine           (const engine&  that) = default;
+  engine           (      engine&& temp) = default;
+  virtual ~engine  ()                    = default;
+  engine& operator=(const engine&  that) = default;
+  engine& operator=(      engine&& temp) = default;
 
 protected:
-
+  frame_timer<float, std::milli> frame_timer_;
 };
 }
 
