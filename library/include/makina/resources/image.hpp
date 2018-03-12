f95771b510493e53524c41f829a55828466d7ee7
demiralp <demiralp@vr.rwth-aachen.de>
2018-03-12 17:57:07 +0100

    Prevent FreeImage Windows header conflicts.

--- library/include/makina/resources/image.hpp
+++ library/include/makina/resources/image.hpp
@@ -1,6 +1,11 @@
 #ifndef MAKINA_RESOURCES_IMAGE_HPP_
 #define MAKINA_RESOURCES_IMAGE_HPP_
 
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#endif
+
 #include <fi/image.hpp>
 
 namespace mak
