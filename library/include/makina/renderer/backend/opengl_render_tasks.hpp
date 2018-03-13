9ddcd2f26b8e9d3e03214c2e304e88a409ad27df
Demiralp <demiralpali@gmail.com>
2018-03-13 02:52:09 +0100

    OpenGL render tasks progress.

--- library/include/makina/renderer/backend/opengl_render_tasks.hpp
+++ library/include/makina/renderer/backend/opengl_render_tasks.hpp
@@ -1,11 +1,18 @@
 #ifndef MAKINA_RENDERER_BACKEND_OPENGL_RENDER_TASKS_HPP_
 #define MAKINA_RENDERER_BACKEND_OPENGL_RENDER_TASKS_HPP_
 
+#include <fg/framegraph.hpp>
+#include <glm/vec4.hpp>
+
 #include <makina/export.hpp>
 
 namespace mak
 {
-  
+struct clear_task_data { };
+struct phong_task_data { };
+
+MAKINA_EXPORT fg::render_task<clear_task_data>* add_clear_render_task(fg::framegraph* framegraph, const glm::vec4& color);
+MAKINA_EXPORT fg::render_task<phong_task_data>* add_phong_render_task(fg::framegraph* framegraph);
 }
 
 #endif
--- library/source/renderer/backend/opengl_render_tasks.cpp
+++ library/source/renderer/backend/opengl_render_tasks.cpp
@@ -1,6 +1,34 @@
 #include <makina/renderer/backend/opengl_render_tasks.hpp>
 
+#include <gl/all.hpp>
+
 namespace mak
 {
-  
+fg::render_task<clear_task_data>* add_clear_render_task(fg::framegraph* framegraph, const glm::vec4& color)
+{
+  return framegraph->add_render_task<clear_task_data>(
+    "Clear Pass",
+    [&] (      clear_task_data& data, fg::render_task_builder& builder)
+    {
+      gl::set_clear_color({color[0], color[1], color[2], color[3]});
+    },
+    [=] (const clear_task_data& data)
+    {
+      gl::clear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    });
+}
+
+fg::render_task<phong_task_data>* add_phong_render_task(fg::framegraph* framegraph)
+{
+  return framegraph->add_render_task<phong_task_data>(
+    "Phong Shading Pass",
+    [&] (      phong_task_data& data, fg::render_task_builder& builder)
+    {
+
+    },
+    [=] (const phong_task_data& data)
+    {
+
+    });
+}
 }
