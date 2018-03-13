28fe1ee6ffa00c5fb16314c54db8165483d2ccb2
demiralp <demiralp@vr.rwth-aachen.de>
2018-03-13 14:10:01 +0100

    Added renderer comments.

--- library/source/renderer/renderer.cpp
+++ library/source/renderer/renderer.cpp
@@ -1,5 +1,10 @@
 #include <makina/renderer/renderer.hpp>
 
+#include <makina/renderer/light.hpp>
+#include <makina/renderer/mesh_render.hpp>
+#include <makina/renderer/projection.hpp>
+#include <makina/renderer/transform.hpp>
+
 namespace mak
 {
 void renderer::prepare()
@@ -8,6 +13,16 @@ void renderer::prepare()
 }
 void renderer::update (frame_timer::duration delta, scene* scene)
 {
+  auto camera       = scene->entities<transform, projection> ();
+  auto lights       = scene->entities<light>                 ();
+  auto mesh_renders = scene->entities<transform, mesh_render>();
+
+  // Backend-independent information:
+  // - Who is the main camera? Or do we create a render target for each camera? If so, what is the viewport of each render target?
+  // - An array of lights per type.
+  // - Vertex, normal, texcoord buffer, index buffers containing data for all meshes.
+  // - Used materials and their relationship to the index buffers.
+
   execute();
 }
 }
