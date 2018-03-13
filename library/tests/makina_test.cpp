6d4af0aa6f4c3bea853287182df8933b298b9454
demiralp <demiralp@vr.rwth-aachen.de>
2018-03-13 11:35:53 +0100

    Clean up test.

--- library/tests/makina_test.cpp
+++ library/tests/makina_test.cpp
@@ -20,28 +20,27 @@
 
 TEST_CASE("Makina test.", "[makina]")
 {
-  struct render_task_data { };
-
   fi::initialize();
   gl::initialize();
 
   auto engine = mak::make_default_engine();
 
-  engine->get_system<mak::display_system>()->create_opengl_window(
+  const auto display_system = engine->get_system<mak::display_system>();
+  display_system->create_opengl_window(
     "Makina", 
     std::array<std::size_t, 2>{32 , 32 }, 
     std::array<std::size_t, 2>{640, 480}, 
     di::opengl_context_settings{di::opengl_profile::core, 4, 2});
 
-  engine->get_system<mak::input_system>()->on_quit.connect(std::bind(&mak::engine::stop, engine.get()));
-  
-  auto renderer = engine->get_system<mak::renderer>();
-  mak::add_clear_render_task(renderer, {0.0F, 0.0F, 0.0F, 1.0F});
-  mak::add_phong_render_task(renderer);
+  const auto input_system = engine->get_system<mak::input_system>();
+  input_system->on_quit.connect(std::bind(&mak::engine::stop, engine.get()));
+
+  const auto renderer = engine->get_system<mak::renderer>();
+  add_clear_render_task(renderer, {0.0F, 0.0F, 0.0F, 1.0F});
+  add_phong_render_task(renderer);
 
   auto& models = mak::registry->get<mak::model>();
-  models.storage().emplace_back();
-  auto& model  = models.storage().back();
+  auto& model  = models.storage().emplace_back();
   ra::load(std::string("data/model/setesh/setesh.obj"), &model);
   engine->set_scene(std::make_unique<mak::scene>(*model.scene));
 
