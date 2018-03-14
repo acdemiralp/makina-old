428c4c56a4436a2d696f4beca8bfac40a6e41e71
Demiralp <demiralpali@gmail.com>
2018-03-14 23:39:52 +0100

    Default engine now adds a default camera and directional light to its scene.

--- library/source/makina.cpp
+++ library/source/makina.cpp
@@ -8,11 +8,29 @@ namespace mak
 {
 std::unique_ptr<engine> make_default_engine()
 {
-  auto   engine = std::make_unique<mak::engine>();
-  engine->set_scene                 (std::make_unique<scene>());
+  auto engine = std::make_unique<mak::engine>();
   engine->add_system<display_system>();
-  engine->add_system<input_system  >();
-  engine->add_system<renderer      >();
+  engine->add_system<input_system>  ();
+  engine->add_system<renderer>      ();
+
+  auto scene = std::make_unique<mak::scene>();
+  {
+    auto entity       = scene ->add_entity();
+    auto transform    = entity->add_component<mak::transform> ();
+    auto projection   = entity->add_component<mak::projection>();
+    auto aspect_ratio = 4.0f / 3.0f;
+    transform ->set_translation(glm::vec3(0.0f, 0.0f, -10.0f));
+    projection->set_perspective(210.0f / aspect_ratio, aspect_ratio, {0.01f, 1000.0f}); // Human horizontal vision is ~210 degrees.
+  }
+  {
+    auto entity    = scene ->add_entity();
+    auto transform = entity->add_component<mak::transform>();
+    auto light     = entity->add_component<mak::light>    ();
+    transform->set_rotation_euler(glm::vec3(50.0f, -30.0f, 0.0f));
+    light->color = glm::vec3(1.0f, 0.95f, 0.83f);
+  }
+  engine->set_scene(std::move(scene));
+
   return engine;
 }
 }
