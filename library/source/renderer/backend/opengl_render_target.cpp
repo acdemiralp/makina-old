7a9d82fc1f63db1f765f6a6febc026034bc2afe2
Demiralp <demiralpali@gmail.com>
2018-03-14 01:41:38 +0100

    Renamed render_target to opengl_render_target.

--- library/source/renderer/backend/opengl_render_target.cpp
+++ library/source/renderer/backend/opengl_render_target.cpp
@@ -1,10 +1,10 @@
-#include <makina/renderer/backend/render_target.hpp>
+#include <makina/renderer/backend/opengl_render_target.hpp>
 
 #include <stdexcept>
 
 namespace mak
 {
-render_target::render_target(const std::array<GLsizei, 2>& size, const GLenum color_format, const GLenum depth_format) : gl::framebuffer()
+opengl_render_target::opengl_render_target(const std::array<GLsizei, 2>& size, const GLenum color_format, const GLenum depth_format) : gl::framebuffer()
 {
   color_texture_.set_storage(0, color_format, size[0], size[1]);
   depth_texture_.set_storage(0, depth_format, size[0], size[1]);
