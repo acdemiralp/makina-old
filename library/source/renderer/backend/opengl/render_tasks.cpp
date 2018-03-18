6e6809a2c6efe90dc0d623e8406c79df0a109efd
Demiralp <demiralpali@gmail.com>
2018-03-18 01:35:31 +0100

    Completed scene loading task.

--- library/source/renderer/backend/opengl/render_tasks.cpp
+++ library/source/renderer/backend/opengl/render_tasks.cpp
@@ -1,5 +1,7 @@
 #include <makina/renderer/backend/opengl/render_tasks.hpp>
 
+#include <optional>
+
 #include <boost/lexical_cast.hpp>
 #include <gl/all.hpp>
 
@@ -67,7 +69,7 @@ fg::render_task<upload_scene_task_data>* add_upload_scene_render_task(renderer* 
       
       data.textures.resize(32);
       for (auto i = 0; i < data.textures.size(); ++i)
-        data.textures[i] = builder.create<texture_2d_resource>("Scene Texture " + boost::lexical_cast<std::string>(i), texture_description{{2048, 2048, 1}, GL_RGBA, 0});
+        data.textures[i] = builder.create<texture_2d_resource>("Scene Texture " + boost::lexical_cast<std::string>(i), texture_description{{2048, 2048, 1}, GL_RGBA8});
       // Totals to 32 * 16.77 = 536 MB of GPU memory for textures.
     },
     [=] (const upload_scene_task_data& data)
@@ -76,36 +78,89 @@ fg::render_task<upload_scene_task_data>* add_upload_scene_render_task(renderer* 
       auto mesh_render_entities = scene->entities<transform, mesh_render>();
       auto light_entities       = scene->entities<transform, light>      ();
       auto camera_entities      = scene->entities<transform, projection> ();
-      auto transforms           = std::vector<_transform>();
-      auto materials            = std::vector<_material> ();
-      auto cameras              = std::vector<_camera>   ();
-      auto lights               = std::vector<_light>    ();
+      auto transforms           = std::vector<_transform>                        ();
+      auto materials            = std::vector<_material>                         ();
+      auto cameras              = std::vector<_camera>                           ();
+      auto lights               = std::vector<_light>                            ();
+      auto draw_calls           = std::vector<gl::draw_elements_indirect_command>();
 
-      for(auto& entity : mesh_render_entities)
+      GLuint texture_offset     = 0;
+      GLuint first_index_offset = 0;
+      GLuint base_vertex_offset = 0;
+      for (auto i = 0; i < mesh_render_entities.size(); ++i)
       {
-        auto transform = entity->component<mak::transform>  ();
-        auto material  = dynamic_cast<phong_material*>(entity->component<mak::mesh_render>()->material);
+        const auto& entity      = mesh_render_entities[i];
+        const auto  transform   = entity->component<mak::transform>  ();
+        const auto  mesh_render = entity->component<mak::mesh_render>();
+
+        auto material = dynamic_cast<phong_material*>(mesh_render->material);
         if (!material) continue;
 
-        transforms.push_back(_transform {transform->matrix(true)});
+        std::optional<gl::texture_handle> ambient_handle, diffuse_handle, specular_handle;
+        if (material->ambient_image)
+        {
+          data.textures[texture_offset]->actual()->set_sub_image(0, 0, 0, material->ambient_image->dimensions()[0], material->ambient_image->dimensions()[1], GL_RGBA, GL_UNSIGNED_BYTE, material->ambient_image->pixels().data);
+          ambient_handle.emplace(*data.textures[texture_offset]->actual());
+          ambient_handle->set_resident(true);
+          texture_offset++;
+        }
+        if (material->diffuse_image)
+        {
+          data.textures[texture_offset]->actual()->set_sub_image(0, 0, 0, material->diffuse_image->dimensions()[0], material->diffuse_image->dimensions()[1], GL_RGBA, GL_UNSIGNED_BYTE, material->diffuse_image->pixels().data);
+          diffuse_handle.emplace(*data.textures[texture_offset]->actual());
+          diffuse_handle->set_resident(true);
+          texture_offset++;
+        }
+        if (material->ambient_image)
+        {
+          data.textures[texture_offset]->actual()->set_sub_image(0, 0, 0, material->specular_image->dimensions()[0], material->specular_image->dimensions()[1], GL_RGBA, GL_UNSIGNED_BYTE, material->specular_image->pixels().data);
+          specular_handle.emplace(*data.textures[texture_offset]->actual());
+          specular_handle->set_resident(true);
+          texture_offset++;
+        }
 
+        transforms.push_back(_transform {transform->matrix(true)});
         materials .push_back(_material  {
           glm::bvec3 
           {
-            material->ambient_image  != nullptr, 
-            material->diffuse_image  != nullptr, 
-            material->specular_image != nullptr
+            ambient_handle ,
+            diffuse_handle ,
+            specular_handle
           },
           material->ambient  ,
           material->diffuse  ,
           material->specular ,
           material->shininess,
-          42, // TODO Load textures first. Pass IDs here.
-          42, // TODO Load textures first. Pass IDs here.
-          42  // TODO Load textures first. Pass IDs here.
+          ambient_handle  ? ambient_handle ->id() : 0,
+          diffuse_handle  ? diffuse_handle ->id() : 0,
+          specular_handle ? specular_handle->id() : 0 
         });
-      }      
-      for(auto& entity : camera_entities)
+
+        const auto& vertices            = mesh_render->mesh->vertices           ;
+        const auto& normals             = mesh_render->mesh->normals            ;
+        const auto& texture_coordinates = mesh_render->mesh->texture_coordinates;
+        const auto& indices             = mesh_render->mesh->indices            ;
+        const std::array<std::uint32_t, 2> instance_attribute {i, i};
+
+        data.vertices           ->actual()->set_sub_data(sizeof vertices           [0] * base_vertex_offset, sizeof vertices           [0] * vertices           .size(), vertices           .data());
+        data.normals            ->actual()->set_sub_data(sizeof normals            [0] * base_vertex_offset, sizeof normals            [0] * normals            .size(), normals            .data());
+        data.texture_coordinates->actual()->set_sub_data(sizeof texture_coordinates[0] * base_vertex_offset, sizeof texture_coordinates[0] * texture_coordinates.size(), texture_coordinates.data());
+        data.indices            ->actual()->set_sub_data(sizeof indices            [0] * base_vertex_offset, sizeof indices            [0] * indices            .size(), indices            .data());
+        data.instance_attributes->actual()->set_sub_data(sizeof std::array<std::uint32_t, 2> * i           , sizeof std::array<std::uint32_t, 2>                       , instance_attribute .data());
+
+        draw_calls.push_back(gl::draw_elements_indirect_command
+        {
+          static_cast<GLuint>(indices.size()),
+          1,
+          first_index_offset,
+          base_vertex_offset,
+          static_cast<GLuint>(i)
+        });
+
+        first_index_offset += static_cast<GLuint>(indices .size());
+        base_vertex_offset += static_cast<GLuint>(vertices.size());
+      }
+      for (auto& entity : camera_entities)
       {
         auto transform  = entity->component<mak::transform> ();
         auto projection = entity->component<mak::projection>();
@@ -114,8 +169,8 @@ fg::render_task<upload_scene_task_data>* add_upload_scene_render_task(renderer* 
           inverse(transform->matrix(true)),
           projection->matrix()
         });
-      }
-      for(auto& entity : light_entities)
+      }     
+      for (auto& entity : light_entities)
       {
         auto transform = entity->component<mak::transform>();
         auto light     = entity->component<mak::light>    ();
@@ -135,52 +190,20 @@ fg::render_task<upload_scene_task_data>* add_upload_scene_render_task(renderer* 
       auto materials_size  = static_cast<GLuint>(materials .size());
       auto cameras_size    = static_cast<GLuint>(cameras   .size());
       auto lights_size     = static_cast<GLuint>(lights    .size());
-      data.transforms->actual()->set_sub_data(0, sizeof glm::uint, &transforms_size);
-      data.materials ->actual()->set_sub_data(0, sizeof glm::uint, &materials_size );
-      data.cameras   ->actual()->set_sub_data(0, sizeof glm::uint, &cameras_size   );
-      data.lights    ->actual()->set_sub_data(0, sizeof glm::uint, &lights_size    );
-      data.transforms->actual()->set_sub_data(sizeof glm::uint, sizeof(glm::mat4) * transforms.size(), transforms.data());
-      data.materials ->actual()->set_sub_data(sizeof glm::uint, sizeof(glm::mat4) * materials .size(), materials .data());
-      data.cameras   ->actual()->set_sub_data(sizeof glm::uint, sizeof(glm::mat4) * cameras   .size(), cameras   .data());
-      data.lights    ->actual()->set_sub_data(sizeof glm::uint, sizeof(glm::mat4) * lights    .size(), lights    .data());
 
-      GLuint first_index_offset = 0;
-      GLuint base_vertex_offset = 0;
-      std::vector<gl::draw_elements_indirect_command> draw_calls;
-      for (auto i = 0; i < mesh_render_entities.size(); ++i)
-      {
-        const auto& entity              = mesh_render_entities[i];
-        const auto  transform           = entity->component<mak::transform>  ();
-        const auto  mesh_render         = entity->component<mak::mesh_render>();
-
-        const auto& vertices            = mesh_render->mesh->vertices           ;
-        const auto& normals             = mesh_render->mesh->normals            ;
-        const auto& texture_coordinates = mesh_render->mesh->texture_coordinates;
-        const auto& indices             = mesh_render->mesh->indices            ;
-        const auto& material            = mesh_render->material                 ;
-        
-        const std::array<std::uint32_t, 2> instance_attribute {i, i}; // TODO Transform index, material index.
-
-        data.vertices           ->actual()->set_sub_data(sizeof vertices           [0] * base_vertex_offset, sizeof vertices           [0] * vertices           .size(), vertices           .data());
-        data.normals            ->actual()->set_sub_data(sizeof normals            [0] * base_vertex_offset, sizeof normals            [0] * normals            .size(), normals            .data());
-        data.texture_coordinates->actual()->set_sub_data(sizeof texture_coordinates[0] * base_vertex_offset, sizeof texture_coordinates[0] * texture_coordinates.size(), texture_coordinates.data());
-        data.instance_attributes->actual()->set_sub_data(sizeof std::array<std::uint32_t, 2> * i           , sizeof std::array<std::uint32_t, 2>                       , instance_attribute .data());
-        data.indices            ->actual()->set_sub_data(sizeof indices            [0] * base_vertex_offset, sizeof indices            [0] * indices            .size(), indices            .data());
-
-        draw_calls.push_back(gl::draw_elements_indirect_command
-        {
-          static_cast<GLuint>(indices .size()),
-          1,
-          first_index_offset,
-          base_vertex_offset,
-          static_cast<GLuint>(i)
-        });
-
-        first_index_offset += static_cast<GLuint>(indices .size());
-        base_vertex_offset += static_cast<GLuint>(vertices.size());
-      }
-      data.draw_calls   ->actual()->set_sub_data(0, sizeof draw_calls[0] * draw_calls.size(), draw_calls.data());
+      data.transforms   ->actual()->set_sub_data(0               , sizeof glm::uint                     , &transforms_size );
+      data.materials    ->actual()->set_sub_data(0               , sizeof glm::uint                     , &materials_size  );
+      data.cameras      ->actual()->set_sub_data(0               , sizeof glm::uint                     , &cameras_size    );
+      data.lights       ->actual()->set_sub_data(0               , sizeof glm::uint                     , &lights_size     );
+      data.transforms   ->actual()->set_sub_data(sizeof glm::uint, sizeof _transform * transforms.size(), transforms.data());
+      data.materials    ->actual()->set_sub_data(sizeof glm::uint, sizeof _material  * materials .size(), materials .data());
+      data.cameras      ->actual()->set_sub_data(sizeof glm::uint, sizeof _camera    * cameras   .size(), cameras   .data());
+      data.lights       ->actual()->set_sub_data(sizeof glm::uint, sizeof _light     * lights    .size(), lights    .data());
+      
+      data.draw_calls   ->actual()->set_sub_data(0           , sizeof gl::draw_elements_indirect_command * draw_calls.size(), draw_calls.data());
       data.parameter_map->actual()->set         ("draw_count", draw_calls.size());
+
+      gl::print_error("Error in Upload Scene Pass: ");
     });
 }
 fg::render_task<clear_task_data>*        add_clear_render_task       (renderer* framegraph, framebuffer_resource* target, const glm::vec4&              color     )
@@ -195,6 +218,8 @@ fg::render_task<clear_task_data>*        add_clear_render_task       (renderer* 
     {
       data.target->actual()->clear_color(std::array<float, 4>{color[0], color[1], color[2], color[3]});
       data.target->actual()->clear_depth_and_stencil(0.0f, 0);
+
+      gl::print_error("Error in Clear Pass: ");
     });
 }
 fg::render_task<phong_task_data>*        add_phong_render_task       (renderer* framegraph, framebuffer_resource* target, const upload_scene_task_data& scene_data)
@@ -247,16 +272,18 @@ fg::render_task<phong_task_data>*        add_phong_render_task       (renderer* 
       data.program     ->actual()->use   ();
       data.vertex_array->actual()->bind  ();
       data.target      ->actual()->bind  ();
-      
+
       gl::set_depth_test_enabled          (true   );
       gl::set_polygon_face_culling_enabled(true   );
       gl::set_front_face                  (GL_CW  );
       gl::set_cull_face                   (GL_BACK);
       gl::multi_draw_elements_indirect    (GL_TRIANGLES, GL_UNSIGNED_INT, 0, data.parameter_map->actual()->get<GLsizei>("draw_count"));
-      
+
       data.target      ->actual()->unbind();
       data.vertex_array->actual()->unbind();
       data.program     ->actual()->unuse ();
+
+      gl::print_error("Error in Phong Shading Pass: ");
     });
 }
 }
