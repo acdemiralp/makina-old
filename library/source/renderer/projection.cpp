4e99d51a6569e014446e9c6b3823cd213e0989fb
Demiralp <demiralpali@gmail.com>
2018-03-20 22:17:34 +0100

    Correct perspective projection to use radians.

--- library/source/renderer/projection.cpp
+++ library/source/renderer/projection.cpp
@@ -6,7 +6,7 @@ namespace mak
 {
 void             projection::set_perspective(const float fov_y, const float aspect_ratio, std::array<float, 2> z_range)
 {
-  matrix_ = glm::perspective(fov_y, aspect_ratio, z_range[0], z_range[1]);
+  matrix_ = glm::perspective(glm::radians(fov_y), aspect_ratio, z_range[0], z_range[1]);
 }
 void             projection::set_orthogonal (const di::rectangle<float> rectangle       , std::array<float, 2> z_range)
 {
