67b9350e5bdc1e06b92c12bdbf6e8c6c82454017
Demiralp <demiralpali@gmail.com>
2018-03-23 02:43:03 +0100

    Refactoring.

--- library/include/makina/renderer/transform.hpp
+++ library/include/makina/renderer/transform.hpp
@@ -20,38 +20,38 @@ public:
   transform& operator=(const transform&  that) = default;
   transform& operator=(      transform&& temp) = default;
 
-  glm::vec3         translation          (const bool absolute = false) const;
-  glm::quat         rotation             (const bool absolute = false) const;
-  glm::vec3         rotation_euler       (const bool absolute = false) const;
-  glm::vec3         scale                (const bool absolute = false) const;
-  glm::mat4         matrix               (const bool absolute = false) const;
-                                         
-  glm::vec3         right                (const bool absolute = false) const;
-  glm::vec3         up                   (const bool absolute = false) const;
-  glm::vec3         forward              (const bool absolute = false) const;
-
-  void              set_translation      (const glm::vec3& translation, const bool absolute = false);
-  void              set_rotation         (const glm::quat& rotation   , const bool absolute = false);
-  void              set_rotation_euler   (const glm::vec3& rotation   , const bool absolute = false);
-  void              set_scale            (const glm::vec3& scale      , const bool absolute = false);
-  void              set_matrix           (const glm::mat4& matrix     , const bool absolute = false);
-                                                                                      
-  void              translate            (const glm::vec3& value      , const bool absolute = false);
-  void              rotate               (const glm::quat& value      , const bool absolute = false);
-  void              rotate_euler         (const glm::vec3& value      , const bool absolute = false);
-  void              scale                (const glm::vec3& value      , const bool absolute = false);
-  void              look_at              (const glm::vec3& target     , const glm::vec3& up = glm::vec3(0.0f, 1.0f, 0.0f), const bool absolute = false);
-  void              reset                ();
-
-  void              set_parent           (transform* parent) override;
-  void              set_parent           (transform* parent, const bool maintain_absolute);
-
-protected:
-  void              update_matrix        ();
-  void              update_hierarchy     ();
+  glm::vec3 translation       (const bool absolute = false) const;
+  glm::quat rotation          (const bool absolute = false) const;
+  glm::vec3 rotation_euler    (const bool absolute = false) const;
+  glm::vec3 scale             (const bool absolute = false) const;
+  glm::mat4 matrix            (const bool absolute = false) const;
+                              
+  glm::vec3 right             (const bool absolute = false) const;
+  glm::vec3 up                (const bool absolute = false) const;
+  glm::vec3 forward           (const bool absolute = false) const;
+            
+  void      set_translation   (const glm::vec3& translation, const bool absolute = false);
+  void      set_rotation      (const glm::quat& rotation   , const bool absolute = false);
+  void      set_rotation_euler(const glm::vec3& rotation   , const bool absolute = false);
+  void      set_scale         (const glm::vec3& scale      , const bool absolute = false);
+  void      set_matrix        (const glm::mat4& matrix     , const bool absolute = false);
+                                                                           
+  void      translate         (const glm::vec3& value      , const bool absolute = false);
+  void      rotate            (const glm::quat& value      , const bool absolute = false);
+  void      rotate_euler      (const glm::vec3& value      , const bool absolute = false);
+  void      scale             (const glm::vec3& value      , const bool absolute = false);
+  void      look_at           (const glm::vec3& target     , const glm::vec3& up = glm::vec3(0.0f, 1.0f, 0.0f), const bool absolute = false);
+  void      reset             ();
+            
+  void      set_parent        (transform* parent) override;
+  void      set_parent        (transform* parent, const bool maintain_absolute);
+            
+protected:  
+  void      update_matrix     ();
+  void      update_hierarchy  ();
 
   glm::vec3 translation_     = glm::vec3(0.0f, 0.0f, 0.0f);
-  glm::quat rotation_        = glm::quat(1.0f, 0.0f, 0.0f, 0.0f);
+  glm::quat rotation_        = glm::quat(glm::vec3(0.0f, 0.0f, 0.0f));
   glm::vec3 scale_           = glm::vec3(1.0f, 1.0f, 1.0f);
   glm::mat4 matrix_          ;
   glm::mat4 absolute_matrix_ ;
