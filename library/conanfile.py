3ee34200587c0b91016eda1b282e934dfac8ce12
Demiralp <demiralpali@gmail.com>
2018-03-23 03:45:47 +0100

    Updated ec version.

--- library/conanfile.py
+++ library/conanfile.py
@@ -17,7 +17,7 @@ class MakinaConan(ConanFile):
                        ("catch2/2.2.0@bincrafters/stable"      ),
                        ("cppzmq/4.2.2@bincrafters/stable"      ),
                        ("di/1.0.3@acdemiralp/makina"           ),
-                       ("ec/1.1.0@acdemiralp/makina"           ),
+                       ("ec/1.1.1@acdemiralp/makina"           ),
                        ("eigen/3.3.4@conan/stable"             ),
                        ("fg/1.0.0@acdemiralp/makina"           ),
                        ("fi/1.0.1@acdemiralp/makina"           ),
