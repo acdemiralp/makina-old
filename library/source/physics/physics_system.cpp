1ecc9517adaee54c499984062666a38e7491a712
saveliyb@gmail.com <saveliyb@gmail.com>
2018-03-25 18:56:20 -0400

    Accounting for added entities in physics system

--- library/source/physics/physics_system.cpp
+++ library/source/physics/physics_system.cpp
@@ -6,60 +6,76 @@
 
 namespace mak
 {
-physics_system::physics_system()
-: configuration_(std::make_unique<btDefaultCollisionConfiguration>    ())
-, dispatcher_   (std::make_unique<btCollisionDispatcher>              (configuration_.get()))
-, broadphase_   (std::make_unique<btDbvtBroadphase>                   ())
-, solver_       (std::make_unique<btSequentialImpulseConstraintSolver>())
-, world_        (std::make_unique<btDiscreteDynamicsWorld>            (dispatcher_.get(), broadphase_.get(), solver_.get(), configuration_.get()))
-{
-  btGImpactCollisionAlgorithm::registerAlgorithm(dispatcher_.get());
+	physics_system::physics_system()
+		: configuration_(std::make_unique<btDefaultCollisionConfiguration>())
+		, dispatcher_(std::make_unique<btCollisionDispatcher>(configuration_.get()))
+		, broadphase_(std::make_unique<btDbvtBroadphase>())
+		, solver_(std::make_unique<btSequentialImpulseConstraintSolver>())
+		, world_(std::make_unique<btDiscreteDynamicsWorld>(dispatcher_.get(), broadphase_.get(), solver_.get(), configuration_.get()))
+	{
+		btGImpactCollisionAlgorithm::registerAlgorithm(dispatcher_.get());
 
-  world_->setGravity(btVector3(0.0f, -9.8f, 0.0f));
-}
+		world_->setGravity(btVector3(0.0f, -9.8f, 0.0f));
+	}
 
-btDiscreteDynamicsWorld* physics_system::world() const
-{
-  return world_.get();
-}
+	btDiscreteDynamicsWorld* physics_system::world() const
+	{
+		return world_.get();
+	}
 
-void physics_system::prepare(scene* scene)
-{
-  for (const auto rigidbody : rigidbodies_)
-    world_->removeRigidBody(rigidbody->native_.get());
-  rigidbodies_.clear();
+	void physics_system::prepare(scene* scene)
+	{
+		for (const auto rigidbody : rigidbodies_)
+			world_->removeRigidBody(rigidbody->native_.get());
 
-  auto entities = scene->entities<transform, mesh_collider, rigidbody>();
-  for(auto entity : entities)
-  {
-    auto transform     = entity->component<mak::transform>    ();
-    auto mesh_collider = entity->component<mak::mesh_collider>();
-    auto rigidbody     = entity->component<mak::rigidbody>    ();
+		rigidbodies_.clear();
+		prepared_entity_ids_.clear();
 
-    btTransform initial_transform;
-    initial_transform.setFromOpenGLMatrix(&transform->matrix(true)[0][0]);
+		prepare_new_entities(scene);
+	}
+	void physics_system::update(frame_timer::duration delta, scene* scene)
+	{
+		prepare_new_entities(scene);
 
-    mesh_collider->native_       = std::make_shared<btConvexHullShape>   (&mesh_collider->mesh()->vertices.data()->x, mesh_collider->mesh()->vertices.size(), sizeof glm::vec3);
-    mesh_collider->motion_state_ = std::make_shared<btDefaultMotionState>(initial_transform);
+		world_->stepSimulation(delta.count());
+		for (auto& object : mesh_colliders_)
+			object.first->set_matrix(object.second->matrix(), true);
+	}
 
-    btVector3 inertia;
-    mesh_collider->native_->calculateLocalInertia(rigidbody->mass(), inertia);
+	void physics_system::prepare_new_entities(scene* scene)
+	{
+		auto entities = scene->entities<transform, mesh_collider, rigidbody>();
 
-    rigidbody->native_ = std::make_shared<btRigidBody>(btRigidBody::btRigidBodyConstructionInfo(
-      rigidbody    ->mass(), 
-      mesh_collider->motion_state_.get(),
-      mesh_collider->native_      .get(),
-      inertia));
-    world_->addRigidBody(rigidbody->native_.get());
+		for (auto entity : entities)
+			if (prepared_entity_ids_.find(entity->id()) == prepared_entity_ids_.end())
+				prepare_entity(entity);
+	}
 
-    rigidbodies_   .push_back(rigidbody);
-    mesh_colliders_.push_back({transform, mesh_collider});
-  }
-}
-void physics_system::update (frame_timer::duration delta, scene* scene)
-{
-  world_->stepSimulation(delta.count());
-  for (auto& object : mesh_colliders_)
-    object.first->set_matrix(object.second->matrix(), true);
-}
+	void physics_system::prepare_entity(entity* entity)
+	{
+		auto transform = entity->component<mak::transform>();
+		auto mesh_collider = entity->component<mak::mesh_collider>();
+		auto rigidbody = entity->component<mak::rigidbody>();
+
+		btTransform initial_transform;
+		initial_transform.setFromOpenGLMatrix(&transform->matrix(true)[0][0]);
+
+		mesh_collider->native_ = std::make_shared<btConvexHullShape>(&mesh_collider->mesh()->vertices.data()->x, mesh_collider->mesh()->vertices.size(), sizeof glm::vec3);
+		mesh_collider->motion_state_ = std::make_shared<btDefaultMotionState>(initial_transform);
+
+		btVector3 inertia;
+		mesh_collider->native_->calculateLocalInertia(rigidbody->mass(), inertia);
+
+		rigidbody->native_ = std::make_shared<btRigidBody>(btRigidBody::btRigidBodyConstructionInfo(
+			rigidbody->mass(),
+			mesh_collider->motion_state_.get(),
+			mesh_collider->native_.get(),
+			inertia));
+		world_->addRigidBody(rigidbody->native_.get());
+
+		rigidbodies_.push_back(rigidbody);
+		mesh_colliders_.push_back({ transform, mesh_collider });
+
+		prepared_entity_ids_.insert(entity->id());
+	}
 }
