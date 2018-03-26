7a99d05e6cf3217888d8a8ef608786dd0997b703
Demiralp <demiralpali@gmail.com>
2018-03-27 00:56:19 +0200

    Vulkan integration progress.

--- library/include/makina/renderer/backend/vulkan/render_tasks.hpp
+++ library/include/makina/renderer/backend/vulkan/render_tasks.hpp
@@ -10,19 +10,11 @@
 #include <vkhlf/Queue.h>
 #include <vkhlf/Surface.h>
 
+#include <makina/aspects/singleton.hpp>
 #include <makina/export.hpp>
 
 namespace mak
 {
-struct MAKINA_EXPORT vulkan_context
-{
-  std::shared_ptr<vkhlf::Instance>             instance       ;
-  std::shared_ptr<vkhlf::DebugReportCallback>  debug_callback ;
-  std::shared_ptr<vkhlf::PhysicalDevice>       physical_device;
-  std::shared_ptr<vkhlf::Device>               logical_device ;
-  std::shared_ptr<vkhlf::Queue>                graphics_queue ;
-};
-
 struct MAKINA_EXPORT window_swapchain
 {
   di::vulkan_window*                           window         ;
@@ -30,7 +22,16 @@ struct MAKINA_EXPORT window_swapchain
   std::unique_ptr<vkhlf::FramebufferSwapchain> swapchain      ;
 };
 
-void create_swapchains(const std::vector<di::window*> windows);
+struct MAKINA_EXPORT vulkan_context : public singleton<vulkan_context>
+{
+  std::shared_ptr<vkhlf::Instance>             instance         ;
+  std::shared_ptr<vkhlf::DebugReportCallback>  debug_callback   ;
+  std::shared_ptr<vkhlf::PhysicalDevice>       physical_device  ;
+  std::shared_ptr<vkhlf::Device>               logical_device   ;
+  std::shared_ptr<vkhlf::Queue>                graphics_queue   ;
+  std::vector<window_swapchain>                window_swapchains;
+};
+
 }
 
 #endif
