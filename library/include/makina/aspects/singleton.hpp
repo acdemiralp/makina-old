29e6f20993ecd82ec5cfd48232e0cfd0ce828f90
Demiralp <demiralpali@gmail.com>
2018-03-27 02:30:01 +0200

    Simplified singleton.

--- library/include/makina/aspects/singleton.hpp
+++ library/include/makina/aspects/singleton.hpp
@@ -1,39 +1,24 @@
 #ifndef MAKINA_ASPECTS_SINGLETON_HPP_
 #define MAKINA_ASPECTS_SINGLETON_HPP_
 
-#include <memory>
-#include <mutex>
 #include <utility>
+
+#include <makina/export.hpp>
 
 namespace mak
 {
 // Note: Do not use a singleton unless you are able to justify that 
 // it is the only optimal solution to the problem you are facing.
 template<typename derived>
-class singleton
+class MAKINA_EXPORT singleton
 {
 public:
   template<typename... argument_types>
   static derived& get(argument_types&&... arguments)
   {
-    static std::once_flag once;
-    std::call_once(once, [ ] (argument_types&&... arguments)
-    {
-      instance_ = std::make_unique<derived>(std::forward<argument_types>(arguments)...);
-    });
-    return *instance_.get();
+    static derived instance(std::forward<argument_types>(arguments)...);
+    return instance;
   }
-
-protected:
-  explicit singleton  ()                       = default;
-  singleton           (const singleton&  that) = delete ;
-  singleton           (      singleton&& temp) = default;
-  virtual ~singleton  ()                       = default;
-  singleton& operator=(const singleton&  that) = delete ;
-  singleton& operator=(      singleton&& temp) = default;
-
-private:
-  static std::unique_ptr<derived> instance_;
 };
 }
 
