14abadcd9a26802b310e0c49e00909acd156fe8d
Demiralp <demiralpali@gmail.com>
2018-03-31 05:25:20 +0200

    Refactoring.

--- library/tests/opengl_test.cpp
+++ library/tests/opengl_test.cpp
@@ -18,7 +18,6 @@ extern "C"
 #include <makina/core/registry.hpp>
 #include <makina/core/scene.hpp>
 #include <makina/display/display_system.hpp>
-#include <makina/input/input_system.hpp>
 #include <makina/renderer/backend/opengl/render_tasks.hpp>
 #include <makina/renderer/light.hpp>
 #include <makina/renderer/renderer.hpp>
@@ -41,9 +40,6 @@ TEST_CASE("OpenGL test.", "[makina]")
   fi::initialize();
   gl::initialize();
 
-  const auto input_system = engine->get_system<mak::input_system>();
-  input_system->on_quit.connect(std::bind(&mak::engine::stop, engine.get()));
-
   const auto renderer   = engine->get_system<mak::renderer>();
   const auto backbuffer = renderer->add_retained_resource("Backbuffer", mak::framebuffer::description(), mak::default_framebuffer());
   const auto upload_scene_render_task = add_upload_scene_render_task            (renderer);
@@ -55,47 +51,15 @@ TEST_CASE("OpenGL test.", "[makina]")
   auto& model  = models.emplace_back();
   model.load(mak::model::description{std::string("data/model/cube/cube.obj"), true});
   
-  std::random_device                    rd  ;
-  std::mt19937                          mt  (rd());
-  std::uniform_real_distribution<float> dist(-0.25f, 0.25f);
+  std::random_device                    random_device   ;
+  std::mt19937                          mersenne_twister(random_device());
+  std::uniform_real_distribution<float> distribution    (-0.25f, 0.25f);
   for(auto i = 0; i < 64; ++i)
   {
     auto entity    = engine->scene()->copy_entity(model.scene->entities()[1]); // TODO: Preserve transform hierarchy when appending / copying.
     auto transform = entity->component<mak::transform>();
-    transform->set_translation(glm::vec3(dist(mt), dist(mt), dist(mt)));
+    transform->set_translation(glm::vec3(distribution(mersenne_twister), distribution(mersenne_twister), distribution(mersenne_twister)));
   }
   
-  //{
-  //  auto entity        = engine->scene()->add_entity();
-  //  auto transform     = entity->add_component<mak::transform>();
-  //  auto light         = entity->add_component<mak::light>    ();
-  //  light->type        = mak::light::type::ambient;
-  //  light->color       = glm::vec3(1.0f, 0.0f, 0.0f);
-  //}
-  //{
-  //  auto entity        = engine->scene()->add_entity();
-  //  auto transform     = entity->add_component<mak::transform>();
-  //  auto light         = entity->add_component<mak::light>    ();
-  //  light->type        = mak::light::type::directional;
-  //  light->color       = glm::vec3(0.0f, 1.0f, 0.0f);
-  //}
-  //{
-  //  auto entity        = engine->scene()->add_entity();
-  //  auto transform     = entity->add_component<mak::transform>();
-  //  auto light         = entity->add_component<mak::light>    ();
-  //  transform->set_translation(glm::vec3(0.0f, 0.0f, 50.0f));
-  //  light->type        = mak::light::type::point;
-  //  light->color       = glm::vec3(0.0f, 0.0f, 1.0f);
-  //}
-  //{
-  //  auto entity        = engine->scene()->add_entity();
-  //  auto transform     = entity->add_component<mak::transform>();
-  //  auto light         = entity->add_component<mak::light>    ();
-  //  transform->set_translation(glm::vec3(0.0f, 0.0f, -10.0f));
-  //  light->type        = mak::light::type::spot;
-  //  light->spot_angles = {5.0f, 15.0f};
-  //  light->color       = glm::vec3(1.0f, 1.0f, 1.0f);
-  //}
-
   engine->run();
 }
