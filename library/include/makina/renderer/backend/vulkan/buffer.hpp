dded83ca63f9583608bd396571b36d1a9a090a0c
Demiralp <demiralpali@gmail.com>
2018-04-04 23:53:18 +0200

    Provide defaults for Vulkan buffers.

--- library/include/makina/renderer/backend/vulkan/buffer.hpp
+++ library/include/makina/renderer/backend/vulkan/buffer.hpp
@@ -16,12 +16,13 @@ namespace vulkan
 {
 struct MAKINA_EXPORT buffer_description
 {
-  vk::BufferCreateFlags               flags                ;
-  vk::DeviceSize                      size                 ;
-  vk::BufferUsageFlags                usage                ;
-  vk::SharingMode                     sharing_mode         ;
-  vk::ArrayProxy<const std::uint32_t> queue_family_indices = nullptr;
-  vk::MemoryPropertyFlags             memory_property_flags;
+  // Rearrangement of members is for convenience.
+  vk::DeviceSize                      size                  ;                        
+  vk::BufferUsageFlags                usage_flags           ;
+  vk::BufferCreateFlags               create_flags          = vk::BufferCreateFlagBits(0);
+  vk::SharingMode                     sharing_mode          = vk::SharingMode::eExclusive;
+  vk::ArrayProxy<const std::uint32_t> queue_family_indices  = nullptr;
+  vk::MemoryPropertyFlags             memory_property_flags = vk::MemoryPropertyFlagBits::eDeviceLocal;
 };
 
 using buffer_resource = fg::resource<buffer_description, std::shared_ptr<vkhlf::Buffer>>;
@@ -32,9 +33,9 @@ template<>
 inline std::unique_ptr<std::shared_ptr<vkhlf::Buffer>> fg::realize(const mak::vulkan::buffer_description& description)
 {
   return std::make_unique<std::shared_ptr<vkhlf::Buffer>>(mak::vulkan::context().logical_device->createBuffer(
-    description.flags                      ,
+    description.create_flags               ,
     description.size                       ,
-    description.usage                      ,
+    description.usage_flags                ,
     description.sharing_mode               ,
     description.queue_family_indices       ,
     description.memory_property_flags      ,
