4bc58ccbfa4a6a9c9ba078b7ba273132093c9270
Demiralp <demiralpali@gmail.com>
2018-05-01 16:28:11 +0200

    Added missing include and refactored C# generator.

--- utility/CsGenerator/CsGenerator/Program.cs
+++ utility/CsGenerator/CsGenerator/Program.cs
@@ -13,19 +13,13 @@ namespace CsGenerator
       driver.Options.GeneratorKind = GeneratorKind.CSharp;
 
       var module = driver.Options.AddModule("Makina");
-
-      var splitIncludeDirs = IncludeDirs.Split(';');
-      var splitIncludes    = Includes   .Split(';');
-      var splitLibraryDirs = LibraryDirs.Split(';');
-      var splitLibraries   = Libraries  .Split(';');
-
-      foreach (var includeDir in splitIncludeDirs)
+      foreach (var includeDir in IncludeDirs.Split(';'))
         module.IncludeDirs.Add(includeDir);
-      foreach (var include    in splitIncludes   )
+      foreach (var include    in Includes   .Split(';'))
         module.Headers    .Add(include   );
-      foreach (var libraryDir in splitLibraryDirs)
+      foreach (var libraryDir in LibraryDirs.Split(';'))
         module.LibraryDirs.Add(libraryDir);
-      foreach (var library    in splitLibraries)
+      foreach (var library    in Libraries  .Split(';'))
         module.Libraries  .Add(library   );
     }
     public void SetupPasses(Driver driver)
@@ -41,7 +35,7 @@ namespace CsGenerator
 
     }
 
-    private const string IncludeDirs = "${PROJECT_SOURCE_DIR}/include;${PROJECT_INCLUDE_DIRS}";
+    private const string IncludeDirs = "${PROJECT_SOURCE_DIR}/include;${PROJECT_INCLUDE_DIRS};${CONAN_INCLUDE_DIRS_RELEASE}";
     private const string Includes    = "${PROJECT_HPPS}";
     private const string LibraryDirs = "${PROJECT_BINARY_DIR}/Release";
     private const string Libraries   = "${PROJECT_NAME}.lib";
