09c225b0bef1e88ef6ed61407b0d15c477fdc625
Demiralp <demiralpali@gmail.com>
2018-05-07 21:43:26 +0200

    Renderer now also caches the delta duration to forward it to render tasks.

--- library/source/renderer/renderer.cpp
+++ library/source/renderer/renderer.cpp
@@ -2,18 +2,23 @@
 
 namespace mak
 {
-const scene* renderer::scene_cache() const
+const frame_timer::duration& renderer::delta_cache() const
+{
+  return delta_cache_;
+}
+const scene*                 renderer::scene_cache() const
 {
   return scene_cache_;
 }
 
-void         renderer::prepare    (scene* scene)
+void renderer::prepare(                             scene* scene)
 {
   compile        ();
   export_graphviz("renderer.gv");
 }
-void         renderer::update     (frame_timer::duration delta, scene* scene)
+void renderer::update (frame_timer::duration delta, scene* scene)
 {
+  delta_cache_ = delta;
   scene_cache_ = scene;
   execute();
 }
