02a1b0100b9dcd72ce662c54dd27055596a4bed2
demiralp <demiralp@vr.rwth-aachen.de>
2018-05-28 17:46:26 +0200

    Scripting system is no longer asynchronous.

--- library/source/default_engine.cpp
+++ library/source/default_engine.cpp
@@ -35,7 +35,6 @@ std::unique_ptr<engine> make_default_engine()
   if (mak::vr_system::available()) engine->add_system<mak::vr_system>       ();
   auto renderer                  = engine->add_system<mak::renderer>        ();
   input_system    ->set_async(true);
-  scripting_system->set_async(true);
   audio_system    ->set_async(true);
   physics_system  ->set_async(true);
   animation_system->set_async(true);
