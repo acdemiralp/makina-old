6ba5bca52e1fea08b88f4e5e24ce5756ccd30500
demiralp <demiralp@vr.rwth-aachen.de>
2018-05-29 17:33:41 +0200

    Refactored mesh.

--- library/include/makina/resources/mesh.hpp
+++ library/include/makina/resources/mesh.hpp
@@ -4,26 +4,24 @@
 #include <cstdint>
 #include <vector>
 
-#include <glm/vec2.hpp>
-#include <glm/vec3.hpp>
-#include <glm/vec4.hpp>
+#include <glm/glm.hpp>
 
 #include <makina/aspects/named.hpp>
 #include <makina/export.hpp>
 
 namespace mak
 {
-struct MAKINA_EXPORT mesh : public named
+struct MAKINA_EXPORT mesh : named
 {
   std::vector<glm::vec3>     vertices           ;
-  std::vector<glm::vec3>     normals            ;
-  std::vector<glm::vec3>     texture_coordinates;
   std::vector<glm::u8vec4>   colors             ;
+  std::vector<glm::vec3>     normals            ;
   std::vector<glm::vec3>     tangents           ;
   std::vector<glm::vec3>     bitangents         ;
-  std::vector<std::uint32_t> indices            ;
+  std::vector<glm::vec3>     texture_coordinates;
   std::vector<glm::uvec4>    bone_ids           ;
   std::vector<glm::vec4>     bone_weights       ;
+  std::vector<std::uint32_t> indices            ;
 };
 }
 
