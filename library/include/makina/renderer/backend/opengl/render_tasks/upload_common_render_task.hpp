5f87653fef51832862377a9023605a616d59a9a0
Demiralp <demiralpali@gmail.com>
2018-05-30 19:26:40 +0200

    Implemented upload common render task.

--- library/include/makina/renderer/backend/opengl/render_tasks/upload_common_render_task.hpp
+++ library/include/makina/renderer/backend/opengl/render_tasks/upload_common_render_task.hpp
@@ -3,17 +3,24 @@
 
 #include <fg/render_task.hpp>
 
+#include <makina/renderer/backend/opengl/buffer.hpp>
+#include <makina/renderer/backend/parameter_map.hpp>
 #include <makina/renderer/renderer.hpp>
 #include <makina/export.hpp>
 
 namespace mak
 {
+namespace opengl
+{
 struct MAKINA_EXPORT upload_common_task_data
 {
-  
+  buffer_resource*        cameras      ;
+  buffer_resource*        lights       ;
+  parameter_map_resource* parameter_map;
 };
 
 MAKINA_EXPORT fg::render_task<upload_common_task_data>* add_upload_common_render_task(renderer* renderer);
+}
 }
 
 #endif
