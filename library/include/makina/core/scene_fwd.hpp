f370288973efecaf15e4c02b1b0041290eed79ae
Demiralp <demiralpali@gmail.com>
2018-06-02 03:50:31 +0200

    Added function_render stubs.

--- library/include/makina/core/scene_fwd.hpp
+++ library/include/makina/core/scene_fwd.hpp
@@ -15,26 +15,27 @@ class scene;
 
 namespace mak
 {
-class  audio_listener;
-class  audio_source  ;
-class  behavior      ;
-struct metadata      ;
-struct controller    ;
-class  mesh_collider ;
-class  rigidbody     ;
-struct animator      ;
-struct bone          ;
-struct light         ;
-struct line_render   ;
-struct mesh_render   ;
-struct point_render  ;
-class  projection    ;
-class  transform     ;
-struct volume_render ;
+class  audio_listener ;
+class  audio_source   ;
+class  behavior       ;
+struct metadata       ;
+struct controller     ;
+class  mesh_collider  ;
+class  rigidbody      ;
+struct animator       ;
+struct bone           ;
+struct function_render;
+struct light          ;
+struct line_render    ;
+struct mesh_render    ;
+struct point_render   ;
+class  projection     ;
+class  transform      ;
+struct volume_render  ;
 
 using behaviors = std::vector<std::shared_ptr<behavior>>;
 
-using entity    = ec::entity<metadata, audio_listener, audio_source, controller, mesh_collider, rigidbody, animator, bone, light, line_render, mesh_render, point_render, projection, transform, volume_render, behaviors>;
+using entity    = ec::entity<metadata, audio_listener, audio_source, controller, mesh_collider, rigidbody, animator, bone, function_render, light, line_render, mesh_render, point_render, projection, transform, volume_render, behaviors>;
 using scene     = ec::scene<entity>;
 }
 
---
+++ library/include/makina/renderer/function_render.hpp
@@ -0,0 +1,14 @@
+#ifndef MAKINA_RENDERER_FUNCTION_RENDER_HPP_
+#define MAKINA_RENDERER_FUNCTION_RENDER_HPP_
+
+#include <makina/export.hpp>
+
+namespace mak
+{
+struct MAKINA_EXPORT function_render
+{
+
+};
+}
+
+#endif
