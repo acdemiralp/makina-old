1fcea6b9709fc0aa4eebc9079cf9c09f2b0ab62c
Demiralp <demiralpali@gmail.com>
2018-06-04 01:09:06 +0200

    Implemented ospray framebuffer resource.

--- library/include/makina/renderer/backend/ospray/framebuffer.hpp
+++ library/include/makina/renderer/backend/ospray/framebuffer.hpp
@@ -1,13 +1,35 @@
 #ifndef MAKINA_RENDERER_BACKEND_OSPRAY_FRAMEBUFFER_HPP_
 #define MAKINA_RENDERER_BACKEND_OSPRAY_FRAMEBUFFER_HPP_
 
+#include <cstdint>
+
 #include <fg/resource.hpp>
+#include <ospray/ospray_cpp.h>
 
 #include <makina/export.hpp>
 
 namespace mak
 {
+namespace ospray
+{
+struct MAKINA_EXPORT framebuffer_description
+{
+  ospcommon::vec2i     size     ; 
+  OSPFrameBufferFormat format   = OSP_FB_SRGBA;
+  std::int32_t         channels = OSP_FB_COLOR;
+  bool                 clear    = true        ;
+};
+using framebuffer_resource = fg::resource<framebuffer_description, ::ospray::cpp::FrameBuffer>;
+}
+}
 
+template<>
+inline std::unique_ptr<ospray::cpp::FrameBuffer> fg::realize(const mak::ospray::framebuffer_description& description)
+{
+  auto framebuffer = std::make_unique<ospray::cpp::FrameBuffer>(description.size, description.format, description.channels);
+  if (description.clear) 
+    framebuffer->clear(description.channels);
+  return framebuffer;
 }
 
 #endif
