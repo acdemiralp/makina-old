4092b87ce8da6bbd97bb3adefd043b432a7c0a7d
demiralp <demiralp@vr.rwth-aachen.de>
2018-06-04 17:03:14 +0200

    Minor refactoring.

--- library/source/renderer/backend/opengl/render_tasks/create_hmd_textures_render_task.cpp
+++ library/source/renderer/backend/opengl/render_tasks/create_hmd_textures_render_task.cpp
@@ -13,11 +13,11 @@ namespace opengl
 fg::render_task<create_hmd_textures_task_data>* add_create_hmd_textures_render_task(renderer* framegraph, di::hmd* hmd)
 {
   const std::array<GLsizei, 2> size {hmd->recommended_render_target_size()[0], hmd->recommended_render_target_size()[1]};
-  const auto left  = framegraph->add_retained_resource<framebuffer::description, framebuffer>("Left  Eye Framebuffer", framebuffer::description{size});
+  const auto left  = framegraph->add_retained_resource<framebuffer::description, framebuffer>("Left Eye Framebuffer" , framebuffer::description{size});
   const auto right = framegraph->add_retained_resource<framebuffer::description, framebuffer>("Right Eye Framebuffer", framebuffer::description{size});
 
   auto render_task = framegraph->add_render_task<create_hmd_textures_task_data>(
-    "Submit HMD Render Pass",
+    "Create HMD Render Pass",
     [&] (      create_hmd_textures_task_data& data, fg::render_task_builder& builder)
     {
       data.left  = builder.write(left );
