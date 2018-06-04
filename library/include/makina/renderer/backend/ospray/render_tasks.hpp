476b070f441cba7408bb6b79c16d5377086e2b12
demiralp <demiralp@vr.rwth-aachen.de>
2018-06-04 17:05:54 +0200

    Added ospray create_hmd_textures render task.

--- library/include/makina/renderer/backend/ospray/render_tasks.hpp
+++ library/include/makina/renderer/backend/ospray/render_tasks.hpp
@@ -3,6 +3,7 @@
 
 #include <di/systems/display/window.hpp>
 
+#include <makina/renderer/backend/ospray/render_tasks/create_hmd_textures_render_task.hpp>
 #include <makina/renderer/backend/ospray/render_tasks/opengl_adapter_render_task.hpp>
 #include <makina/renderer/backend/ospray/render_tasks/pathtracing_render_task.hpp>
 #include <makina/renderer/backend/ospray/render_tasks/raytracing_render_task.hpp>
