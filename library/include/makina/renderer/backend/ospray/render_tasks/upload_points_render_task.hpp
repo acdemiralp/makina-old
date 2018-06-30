92e96751afe9fedb3819b8b3ee7432e48619dc3b
acdemiralp <demiralpali@gmail.com>
2018-06-30 23:00:17 +0200

    Completed Ospray upload points render task.

--- library/include/makina/renderer/backend/ospray/render_tasks/upload_points_render_task.hpp
+++ library/include/makina/renderer/backend/ospray/render_tasks/upload_points_render_task.hpp
@@ -1,15 +1,33 @@
 #ifndef MAKINA_RENDERER_BACKEND_OSPRAY_RENDER_TASKS_UPLOAD_POINTS_RENDER_TASK_HPP_
 #define MAKINA_RENDERER_BACKEND_OSPRAY_RENDER_TASKS_UPLOAD_POINTS_RENDER_TASK_HPP_
 
-#include <fg/render_task.hpp>
+#include <memory>
 
+#include <fg/render_task.hpp>
+#include <ospray/ospray_cpp.h>
+
+#include <makina/renderer/backend/ospray/buffer.hpp>
+#include <makina/renderer/renderer.hpp>
+#include <makina/resources/point_cloud.hpp>
 #include <makina/export.hpp>
 
 namespace mak
 {
 namespace ospray
 {
-  
+struct MAKINA_EXPORT upload_points_task_data
+{
+  struct MAKINA_EXPORT point_data
+  {
+    std::unique_ptr<::ospray::cpp::Geometry> geometry;
+    std::unique_ptr<::ospray::cpp::Data>     vertices;
+    std::unique_ptr<::ospray::cpp::Data>     colors  ;
+  };
+
+  std::map<point_cloud*, point_data> point_cloud_cache;
+};
+
+MAKINA_EXPORT fg::render_task<upload_points_task_data>* add_upload_points_render_task(renderer* renderer, bool only_raytracing = true);
 }
 }
 
