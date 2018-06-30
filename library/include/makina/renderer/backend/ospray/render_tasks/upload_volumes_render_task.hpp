a2856c8ce5fd04c046786479fabfc143b218ff82
acdemiralp <demiralpali@gmail.com>
2018-07-01 01:18:28 +0200

    Completed Ospray upload volumes task.

--- library/include/makina/renderer/backend/ospray/render_tasks/upload_volumes_render_task.hpp
+++ library/include/makina/renderer/backend/ospray/render_tasks/upload_volumes_render_task.hpp
@@ -1,15 +1,35 @@
 #ifndef MAKINA_RENDERER_BACKEND_OSPRAY_RENDER_TASKS_UPLOAD_VOLUMES_RENDER_TASK_HPP_
 #define MAKINA_RENDERER_BACKEND_OSPRAY_RENDER_TASKS_UPLOAD_VOLUMES_RENDER_TASK_HPP_
 
-#include <fg/render_task.hpp>
+#include <map>
+#include <memory>
 
+#include <fg/render_task.hpp>
+#include <ospray/ospray_cpp.h>
+
+#include <makina/renderer/renderer.hpp>
+#include <makina/resources/field.hpp>
 #include <makina/export.hpp>
 
 namespace mak
 {
 namespace ospray
 {
-  
+struct MAKINA_EXPORT upload_volumes_task_data
+{
+  struct MAKINA_EXPORT volume_data
+  {
+    std::shared_ptr<::ospray::cpp::Volume>           volume           ;
+    std::shared_ptr<::ospray::cpp::Data>             voxel_data       ;
+    std::shared_ptr<::ospray::cpp::TransferFunction> transfer_function;
+    std::shared_ptr<::ospray::cpp::Data>             colors           ;
+    std::shared_ptr<::ospray::cpp::Data>             opacities        ;
+  };
+
+  std::map<field<float, 3>*, volume_data> volume_cache;
+};
+
+MAKINA_EXPORT fg::render_task<upload_volumes_task_data>* add_upload_volumes_render_task(renderer* renderer, bool only_raytracing = true);
 }
 }
 
