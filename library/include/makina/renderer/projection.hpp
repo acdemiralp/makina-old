04d768c5b5bc603ae12925184f40de6cfe5f0975
acdemiralp <demiralpali@gmail.com>
2018-06-30 22:50:02 +0200

    Refactored projection.

--- library/include/makina/renderer/projection.hpp
+++ library/include/makina/renderer/projection.hpp
@@ -14,43 +14,41 @@ namespace mak
 class MAKINA_EXPORT projection
 {
 public:
-  enum class projection_mode
+  enum class mode
   {
     custom      ,
     perspective ,
     orthographic,
     frustum     // For HMDs.
   };
-
-  struct perspective
+  struct MAKINA_EXPORT perspective
   {
     float                fov_y       ;
     float                aspect_ratio;
     std::array<float, 2> z_range     ;
   };
-  struct orthographic
+  struct MAKINA_EXPORT orthographic
   {
     di::rectangle<float> rectangle   ;
     std::array<float, 2> z_range     ;
   };
-  struct frustum
+  struct MAKINA_EXPORT frustum
   {
     di::rectangle<float> rectangle   ;
     std::array<float, 2> z_range     ;
   };
-  using  parameter_type = std::variant<perspective, orthographic, frustum>;
 
-  void             set_perspective (const float fov_y, const float aspect_ratio, std::array<float, 2> z_range);
-  void             set_orthographic(const di::rectangle<float> rectangle       , std::array<float, 2> z_range);
-  void             set_frustum     (const di::rectangle<float> rectangle       , std::array<float, 2> z_range);
-
-  parameter_type   parameters      () const;
-  projection_mode  mode            () const;
-  const glm::mat4& matrix          () const;
+  void                                             set_perspective (const float fov_y, const float aspect_ratio, std::array<float, 2> z_range);
+  void                                             set_orthographic(const di::rectangle<float> rectangle       , std::array<float, 2> z_range);
+  void                                             set_frustum     (const di::rectangle<float> rectangle       , std::array<float, 2> z_range);
+  
+  mode                                             mode            () const;
+  std::variant<perspective, orthographic, frustum> parameters      () const;
+  const glm::mat4&                                 matrix          () const;
 
 protected:
-  parameter_type parameters_;
-  glm::mat4      matrix_    ;
+  std::variant<perspective, orthographic, frustum> parameters_;
+  glm::mat4                                        matrix_    ;
 };
 }
 
