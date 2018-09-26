4daeca44c4ec3109d9a56bd2eb11461fe79f854d
demiralp <demiralp@vr.rwth-aachen.de>
2018-09-26 11:17:18 +0200

    Scripting system no longer executes behaviors in parallel among entities since they may modify the same resources.

--- library/source/scripting/scripting_system.cpp
+++ library/source/scripting/scripting_system.cpp
@@ -9,21 +9,17 @@ namespace mak
 void scripting_system::prepare(                                   scene* scene)
 {
   const auto entities = scene->entities<behaviors>();
-  tbb::parallel_for(std::size_t(0), entities.size(), std::size_t(1), [=] (std::size_t i)
-  {
-    for (auto& behavior : *entities[i]->component<behaviors>())
+  for (auto entity : entities)
+    for (auto& behavior : *entity->component<behaviors>())
       if (behavior->on_prepare_)
-        behavior->on_prepare_(scene, entities[i]);
-  });
+        behavior->on_prepare_(scene, entity);
 }
 void scripting_system::update (const frame_timer::duration delta, scene* scene)
 {
   const auto entities = scene->entities<behaviors>();
-  tbb::parallel_for(std::size_t(0), entities.size(), std::size_t(1), [=] (std::size_t i)
-  {
-    for (auto& behavior : *entities[i]->component<behaviors>())
+  for (auto entity : entities)
+    for (auto& behavior : *entity->component<behaviors>())
       if (behavior->on_update_)
-        behavior->on_update_(delta, scene, entities[i]);
-  });
+        behavior->on_update_(delta, scene, entity);
 }
 }
