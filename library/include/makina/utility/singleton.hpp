605cbcd46c81622642d705b62ad32de5cbdb95a8
demiralp <demiralp@vr.rwth-aachen.de>
2018-09-26 12:44:20 +0200

    Added singleton.

---
+++ library/include/makina/utility/singleton.hpp
@@ -0,0 +1,28 @@
+#ifndef MAKINA_UTILITY_SINGLETON_HPP_
+#define MAKINA_UTILITY_SINGLETON_HPP_
+
+#include <utility>
+
+namespace mak
+{
+template <typename derived>
+class singleton
+{
+public:
+  singleton           ()                       = default;
+  singleton           (const singleton&  that) = delete ;
+  singleton           (      singleton&& temp) = delete ;
+  virtual ~singleton  ()                       = default;
+  singleton& operator=(const singleton&  that) = delete ;
+  singleton& operator=(      singleton&& temp) = delete ;
+
+  template <typename... argument_types>
+  static derived& instance(argument_types&&... arguments)
+  {
+    static derived instance(std::forward<argument_types>(arguments...));
+    return instance;
+  }
+};
+}
+
+#endif
