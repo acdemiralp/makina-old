1bd667b83f9256bfe629eb0eb2df976f5d8da0de
demiralp <demiralp@vr.rwth-aachen.de>
2018-09-26 14:40:47 +0200

    If a key does not exist in the configuration, add it.

--- library/include/makina/core/configuration.hpp
+++ library/include/makina/core/configuration.hpp
@@ -21,7 +21,8 @@ public:
   template<typename type>
   type get(const std::string& key, const type default_value = type())
   {
-    return json_.count(key) ? json_[key].get<type>() : default_value;
+    if (!json_.count(key)) json_[key] = default_value;
+    return json_[key].get<type>();
   }
   template<typename type>
   void set(const std::string& key, const type value)
